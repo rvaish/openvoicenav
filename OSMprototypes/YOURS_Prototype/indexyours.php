0e8432b9dd32337a3d27583fe54c9f5766fd15cc
Rajan <Rajan@generoushous.juitw.org>
2009-07-14 01:33:45 +0530

    added YOURS_Prototype

---
+++ OSMprototypes/YOURS_Prototype/indexyours.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<form action="routingyours.php" method="get">
+<center><h3>YOURS & ORS for Visually Impaired(Simple prototype)</h3></center><br>
+<center><b> Source </b> (e.g. Street,City,Country) <input type="text" name="source" />
+
+  <b> Destination </b> (e.g. Street,City,Country) <input type="text" name="desti" /></center>
+<br>
+<small><center>Tip: Use country,in query to avoid dublicacy and better results.</small></center>
+<br>
+<center><input type="submit" /></center>
+</form>
+<center><a href="http://localhost/yours/indexyours.php">Refresh</a>
+<a href="http://openstreetmap.org">OSM</a>
+<a href="http://www.yournavigation.org/">YOURS</a>
+<a href="http://openrouteservice.org">ORS</a></center>
+<br>
+<br>
+<small><center>Geocoding:ORS - Routing Engine:YOURS/GOSMORE</center></small>
+</body>
+</html>
