8931c10b0804ac3ebdb1fb4df36e04731dbbfb9b
Rajan <Rajan@generoushous.juitw.org>
2009-07-29 04:42:00 +0530

    added folder ORS_Python updated folder orsgae

--- OSMprototypes/orsgae/orsroute.py
+++ OSMprototypes/orsgae/orsroute.py
@@ -12,9 +12,15 @@ class MainPage(webapp.RequestHandler):
       <html>
         <body>
           <form action="/ors" method="get">
-            <div><input type="text" name="source">
-			<input type="text" name="desti">
-            <input type="submit" value="Search"></div>
+		    <center><h3> ORS for Visually Impaired(Simple Prototype) - <sup>powered by Google App Engine</sup> </h3></center>
+            <div>
+			<center>
+			<b>Source</b> (Street, City, Country) :<input type="text" name="source">
+            <b>Destination</b> (Street, City, Country) :<input type="text" name="desti">
+			</center>
+			<br>
+            <center><input type="submit" value="Search"></center>
+			</div>
           </form>
         </body>
       </html>""")
@@ -23,14 +29,14 @@ class Orsroute(webapp.RequestHandler):
     def get(self):
         sourceori = self.request.get('source')
         destiori = self.request.get('desti')
-        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write('<html><body>Routing Information is:<pre>')
         self.response.out.write(sourceori+'<br/>')
         self.response.out.write(destiori+'<br/><br/>')
         source = urllib.quote(sourceori.encode('utf8'))
         desti = urllib.quote(destiori.encode('utf8'))
         url_orsnamefinder = "http://data.giub.uni-bonn.de/openrouteservice/php/Geocode_rajan.php?FreeFormAdress="
         url = url_orsnamefinder + source + "&MaxResponse=1"
-        url1 = url_orsnamefinder + desti + "&MaxResponse=1"	  
+        url1 = url_orsnamefinder + desti + "&MaxResponse=1"       
 
         # FOR SOURCE
         DOMTree = minidom.parseString(fetch(url).content)
@@ -52,7 +58,7 @@ class Orsroute(webapp.RequestHandler):
             longdesti = latlong1[0:spacepos1]
             latdesti = latlong1[spacepos1+1:len(latlong1)]
 
-	# FINAL CALCULATION OF ROUTES
+        # FINAL CALCULATION OF ROUTES
         url_source = longsource + "," + latsource
         url_desti = longdesti + "," + latdesti
         # output lat,lon
@@ -61,13 +67,13 @@ class Orsroute(webapp.RequestHandler):
         self.response.out.write('<br/><br/>')
         url_route = "http://data.giub.uni-bonn.de/openrouteservice/php/DetermineRoute_rajan.php?Start=" + url_source + "&Via=&End=" + url_desti + "&lang=en&distunit=KM&routepref=Fastest&avoidAreas=&useTMC=false&noMotorways=false&noTollways=false&instructions=true"
 
-	# Open XML document using minidom parser
+        # Open XML document using minidom parser
         DOMTree2 = minidom.parseString(fetch(url_route).content)
-	hmm2 = DOMTree2.getElementsByTagNameNS("http://www.opengis.net/xls","RouteInstruction")
+        hmm2 = DOMTree2.getElementsByTagNameNS("http://www.opengis.net/xls","RouteInstruction")
 
-	for RouteInstruction in hmm2:
+        for RouteInstruction in hmm2:
             type2 = RouteInstruction.getElementsByTagNameNS("http://www.opengis.net/xls","Instruction")[0]
-            self.response.out.write(cgi.escape(type2.childNodes[0].data)+'&nbsp;&nbsp;&nbsp;')
+            self.response.out.write(cgi.escape(type2.childNodes[0].data)+'   ')
             type3 = RouteInstruction.getElementsByTagNameNS("http://www.opengis.net/xls","distance")[0]
             self.response.out.write(cgi.escape(type3.getAttribute('value'))+'<br/>')
 
@@ -82,4 +88,4 @@ def main():
   run_wsgi_app(application)
 
 if __name__ == "__main__":
-  main()   
+  main() 
