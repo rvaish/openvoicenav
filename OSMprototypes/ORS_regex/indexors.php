e582b1cb95f92414daf54b7a5ca43df8fdd38e48
Rajan <Rajan@generoushous.juitw.org>
2009-08-04 04:38:49 +0530

    added folder ORS_regex

---
+++ OSMprototypes/ORS_regex/indexors.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<form action="routingors.php" method="get">
+<center><h3>ORS for Visually Impaired (Simple prototype)</h3></center><br>
+<center><b> Source </b> (e.g. Street,City) <input type="text" name="source" />
+
+  <b> Destination </b> (e.g. Street,City) <input type="text" name="desti" /></center>
+<br>
+<small><center>Tip : Narrow down search by adding Country in query (when dublicacy exists).</center></small>
+<br>
+<center><input type="submit" /></center>
+</form>
+<center><a href="http://localhost/ors/indexors.php">Refresh</a>
+<a href="http://openstreetmap.org">OSM</a>
+<a href="http://openrouteservice.org/">ORS</a></center>
+<br><br>
+<small><center>Geocoding:ORS - Routing Engine:ORS</center></small>
+</body>
+</html>
